8e6238b17b4112a34aeef9d9f352ce7a711c7331
sreekanths-24 <sreekanthsanthosh17@gmail.com>
2022-10-11 20:39:08 +0530

    commited to add files to github

--- guess_the_number.py
+++ guess_the_number.py
@@ -3,16 +3,16 @@
 import random
 
 def guess():
-    numberRange = int(input("Guess a number between 1 to "))
+    numberRange = int(input("\nLet me choose a secret number between 1 to "))
     secretNumber = random.randint(1, numberRange)
     userGuess = 0
     while userGuess != secretNumber:
-        userGuess = int(input("Guess the secret number: ")) 
+        userGuess = int(input("\nGuess the secret number: ")) 
 
         if userGuess < secretNumber:
             print("Sorry, try again. Too low.")
         elif userGuess > secretNumber:
             print("Sorry, try again. Too high.")
-    print(f"Congrats you guessed the secret number {secretNumber} correctly!!!")
+    print(f"\nCongrats you guessed the secret number {secretNumber} correctly!!!")
 
 guess()
