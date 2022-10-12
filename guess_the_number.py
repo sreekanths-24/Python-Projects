270fe48caf1c65a0248200d5f7be097d2e58d314
sreekanths-24 <sreekanthsanthosh17@gmail.com>
2022-10-12 20:54:21 +0530

    Added number guessing by computer

--- guess_the_number.py
+++ guess_the_number.py
@@ -2,10 +2,11 @@
 
 import random
 
-def guess():
+def userGuess():
     numberRange = int(input("\nLet me choose a secret number between 1 to "))
     secretNumber = random.randint(1, numberRange)
     userGuess = 0
+    numberOfSteps = 0
     while userGuess != secretNumber:
         userGuess = int(input("\nGuess the secret number: ")) 
 
@@ -13,6 +14,36 @@ def guess():
             print("Sorry, try again. Too low.")
         elif userGuess > secretNumber:
             print("Sorry, try again. Too high.")
-    print(f"\nCongrats you guessed the secret number {secretNumber} correctly!!!")
+        numberOfSteps += 1
+    print(f"\nCongrats you guessed the secret number {secretNumber} in {numberOfSteps} steps!!")
 
-guess()
+
+def computerGuess():
+    low = int(input("Tell me the lower limit of your secret number : "))
+    high = int(input("Tell me the upper limit of your secret number : "))
+    result = 0
+    numOfSteps = 0
+    
+    if low == high:
+        guess = low
+
+    else:
+        while result != "c":
+            guess = random.randrange(low, high)
+            
+
+            result = str(input(f"\nIs the number in your mind {guess} ? too high =\"h\", too low =\"l\", correct guess =\"c\": "))
+
+            if result == "h":
+                high = guess
+            elif result == "l":
+                low = guess
+
+            if low == high:
+                guess = low
+            
+            numOfSteps += 1
+    print(f"The computer guessed your secret number is {guess} in {numOfSteps} steps!!")
+
+#computerGuess()
+#userGuess()
